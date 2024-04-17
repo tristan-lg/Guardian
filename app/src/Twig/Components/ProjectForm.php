a5525e4fcea95b87c47ae6cfdeb75ffb4dbd9f61
Tristan LE GACQUE <t.le-gacque@groupeonepoint.com>
2024-04-17 14:55:37 +0200

    Add live component

--- app/src/Twig/Components/ProjectForm.php
+++ app/src/Twig/Components/ProjectForm.php
@@ -8,5 +8,10 @@ use Symfony\UX\LiveComponent\DefaultActionTrait;
 #[AsLiveComponent]
 final class ProjectForm
 {
+    public function getRandomNumber(): int
+    {
+        return rand(0, 1000);
+    }
+    
     use DefaultActionTrait;
 }
