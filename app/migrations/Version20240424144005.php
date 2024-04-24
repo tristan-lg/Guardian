80cb265da1f6ebd4147593e187f2a9c1c72b4733
Tristan LE GACQUE <t.le-gacque@groupeonepoint.com>
2024-04-24 16:40:16 +0200

    Add discord notification Add loading on new project

--- app/migrations/Version20240424144005.php
+++ app/migrations/Version20240424144005.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240424130143 extends AbstractMigration
+final class Version20240424144005 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,10 @@ final class Version20240424130143 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE advisory ADD advisory_id VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE advisory ADD CONSTRAINT FK_4112BDD97941003F FOREIGN KEY (analysis_id) REFERENCES analysis (id)');
         $this->addSql('CREATE INDEX IDX_4112BDD97941003F ON advisory (analysis_id)');
+        $this->addSql('ALTER TABLE analysis ADD advisory_hash VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -29,5 +31,7 @@ final class Version20240424130143 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE advisory DROP FOREIGN KEY FK_4112BDD97941003F');
         $this->addSql('DROP INDEX IDX_4112BDD97941003F ON advisory');
+        $this->addSql('ALTER TABLE advisory DROP advisory_id');
+        $this->addSql('ALTER TABLE analysis DROP advisory_hash');
     }
 }
