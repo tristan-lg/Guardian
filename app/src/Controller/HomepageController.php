ee08d0cb90865acd2b15865ecbd0a1740c4ad465
Tristan LE GACQUE <t.le-gacque@groupeonepoint.com>
2025-11-17 14:58:44 +0100

    secure homepage

--- app/src/Controller/HomepageController.php
+++ app/src/Controller/HomepageController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use App\Entity\Project;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, #[Autowire('%app.homepage_require_login%')] bool $homepageRequireLogin): Response
     {
+        if ($homepageRequireLogin) {
+            $this->denyAccessUnlessGranted('ROLE_USER');
+        }
+
         return $this->render('@Pages/homepage.html.twig', [
             'projects' => $em->getRepository(Project::class)->getProjectOrderedByGrade(),
         ]);
