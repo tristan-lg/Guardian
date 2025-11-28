2f264cd585d8878352796a97eb812421465297c1
Tristan LE GACQUE <t.le-gacque@groupeonepoint.com>
2025-11-28 16:57:32 +0100

    Add secureLink service

---
+++ app/src/Controller/Security/TwoFactor/EnableTwoFactorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Security\TwoFactor;
+
+use App\Entity\User;
+use App\Form\EnableTwoFactorFormType;
+use App\Service\Security\TwoFactorService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/activation-2fa')]
+class EnableTwoFactorController extends AbstractController
+{
+    public function __construct(
+        private readonly TwoFactorService $twoFactorService,
+    ) {}
+
+    #[Route('', name: 'app_2fa_enable')]
+    public function index(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        if ($user->isTotpAuthenticationEnabled()) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $user->setTotpSecret($this->twoFactorService->generateTwoFactorSecret());
+        $form = $this->createForm(EnableTwoFactorFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $topSecretToken */
+            $topSecretToken = $user->getTotpSecret();
+            $this->twoFactorService->setUserTwoFactorSecret($user, $topSecretToken);
+
+            // Enable 2FA
+            $this->addFlash('success', 'La double authentification a bien été activée');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('security/two-factor/enable-2fa.html.twig', [
+            'form' => $form,
+            'qrCode' => $this->twoFactorService->getQrCodeContent($user),
+            'secretCode' => $user->getTotpSecret(),
+        ]);
+    }
+}
